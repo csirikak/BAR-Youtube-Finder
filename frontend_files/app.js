63f150e52e1da8e30d1821b4450d1a6ff2a62239
csirikak <bit.happyc@gmail.com>
2025-11-21 20:37:01 -0800

    update 2025-11-21

--- frontend_files/app.js
+++ frontend_files/app.js
@@ -463,9 +463,9 @@ document.addEventListener('DOMContentLoaded', () => {
             
             return `
                 <tr>
-                    <td class="col-thumb"><a href="${ytLink}" target="_blank"><img src="${thumbUrl}" alt="Thumbnail"></a></td>
+                    <td class="col-thumb"><a href="${ytLink}&t=${item.timestamp}s" target="_blank"><img src="${thumbUrl}" alt="Thumbnail"></a></td>
                     <td class="col-title">
-                        <a href="${ytLink}" target="_blank"><strong>${item.title}</strong></a>
+                        <a href="${ytLink}&t=${item.timestamp}s" target="_blank"><strong>${item.title}</strong></a>
                         <p>Matched: ${item.ocr_name}</p>
                         <p>Uploader: ${item.uploader}</p>
                     </td>
@@ -524,9 +524,9 @@ document.addEventListener('DOMContentLoaded', () => {
             
             tableRows.push(`
                 <tr>
-                    <td class="col-thumb"><a href="${ytLink}" target="_blank"><img src="${thumbUrl}" alt="Thumbnail"></a></td>
+                    <td class="col-thumb"><a href="${ytLink}&t=${item.timestamp}s" target="_blank"><img src="${thumbUrl}" alt="Thumbnail"></a></td>
                     <td class="col-title">
-                        <a href="${ytLink}" target="_blank"><strong>${match.title}</strong></a>
+                        <a href="${ytLink}&t=${item.timestamp}s" target="_blank"><strong>${match.title}</strong></a>
                         <p>Found at: ${formatTimestamp(match.timestamp)}</p>
                         <p>Uploader: ${match.uploader}</p>
                     </td>
