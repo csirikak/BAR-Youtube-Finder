cd93a5bf50b5a1cb51594ec72b2e542c962a46e5
csirikak <bit.happyc@gmail.com>
2026-03-05 20:36:05 -0800

    update 2026-03-05

--- scrape.py
+++ scrape.py
@@ -226,8 +226,8 @@ def process_video_screenshots(video, output_dir, game_tag=""):
             ffmpeg_command.extend(['-headers', header_string])
 
         ffmpeg_command.extend([
-            '-hwaccel', 'vaapi',                 # 1. Force VAAPI
-            '-hwaccel_device', '/dev/dri/renderD128', # 2. Select the AMD GPU
+            '-hwaccel', 'cuda',                 # 1. Force cuda
+            '-hwaccel_device', '/dev/dri/renderD128', # 2. Select the CUDA GPU
             '-hwaccel_output_format', 'yuv420p', # 3. Copy frame from GPU to CPU
             '-ss', str(timestamp_sec), 
             '-i', stream_url,
